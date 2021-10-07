1326bb48278b82a2f7a4bac98d91812773b1c7f7
Gustavo Siqueira <gosiqueira@dcc.ufmg.br>
2021-10-07 20:32:55 -0300

    Fix missing #48 test cases

--- matrices/0048_rotate_image.py
+++ matrices/0048_rotate_image.py
@@ -29,3 +29,37 @@ matrix[i].length == n
 1 <= n <= 20
 -1000 <= matrix[i][j] <= 1000
 """
+
+from typing import List
+
+
+def rotate(matrix: List[List[int]]) -> None:
+    """
+    Time: O(n^2)
+    Space: O(1)
+    """
+    for layer in range(len(matrix) // 2):
+            first = layer
+            last = len(matrix) - layer - 1
+
+            for i in range(first, last):
+                offset = i - first
+                
+                top = matrix[first][i]
+                
+                matrix[first][i] = matrix[last - offset][first]
+                matrix[last - offset][first] = matrix[last][last - offset]
+                matrix[last][last - offset] = matrix[i][last]
+                matrix[i][last] = top
+
+
+if __name__ == '__main__':
+    # Test 1
+    matrix = [[1,2,3],[4,5,6],[7,8,9]]
+    rotate(matrix)
+    print(matrix)
+
+    # Test 2
+    matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+    rotate(matrix)
+    print(matrix)
